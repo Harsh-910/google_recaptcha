2c718aa40627677975904b62d21cf70bb953fcdf
Harsh-910 <97285448+Harsh-910@users.noreply.github.com>
2022-02-07 18:39:56 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./styles.css";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function App() {
+  const onChange = (value) => {
+    console.log("Captcha value:", value);
+  };
   return (
     <div className="App">
       <h1>Hello Google Recaptcha</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <ReCAPTCHA
+        // type="image"
+        sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+        onChange={onChange}
+      />
     </div>
   );
 }
